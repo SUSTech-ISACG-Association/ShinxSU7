08022703364d0ad5198b021dc508bee33c8d1439
GuTaoZi <gutao.official@gmail.com>
2024-12-19 23:26:42 +0800

    update waypoint UI

--- LeiJun/Core/Src/bluetooth.c
+++ LeiJun/Core/Src/bluetooth.c
@@ -47,28 +47,27 @@ void send_greeting()
     uint8_t opcode = 0x00;
     uint8_t receive_buffer;
     float distance = 0;
-	LCD_ShowString(10, 290, 100, 16, 16, "send 0x00");
-    HAL_UART_Transmit(huart, &opcode, 1, 0xffff);        // send Greeting
-	LCD_ShowString(10, 290, 100, 16, 16, "recv Ack2");
+    LCD_ShowString(10, 290, 100, 16, 16, "send 0x00");
+    HAL_UART_Transmit(huart, &opcode, 1, 0xffff); // send Greeting
+    LCD_ShowString(10, 290, 100, 16, 16, "recv Ack2");
     HAL_UART_Receive(huart, &receive_buffer, 1, 0xffff); // recv ACK2
-	LCD_ShowString(10, 290, 100, 16, 16, "wait Ack2");
-    handle_ack2(receive_buffer);                         // TODO: handle error ACK2
+    LCD_ShowString(10, 290, 100, 16, 16, "wait Ack2");
+    handle_ack2(receive_buffer); // TODO: handle error ACK2
     opcode = 0x80;
-	LCD_ShowString(10, 290, 100, 16, 16, "send 0x80");
-    HAL_UART_Transmit(huart, &opcode, 1, 0xffff);        // send GET distance
-	LCD_ShowString(10, 290, 100, 16, 16, "recv ack1");
+    LCD_ShowString(10, 290, 100, 16, 16, "send 0x80");
+    HAL_UART_Transmit(huart, &opcode, 1, 0xffff); // send GET distance
+    LCD_ShowString(10, 290, 100, 16, 16, "recv ack1");
     HAL_UART_Receive(huart, &receive_buffer, 1, 0xffff); // recv ACK1
                                                          // TODO: handle error ACK1
-	LCD_ShowString(10, 290, 100, 16, 16, "recv dist");
-    HAL_UART_Receive(huart, &distance, 4, 0xffff);       // recv distance data
-	LCD_ShowString(10, 290, 100, 16, 16, "recv ack2");
+    LCD_ShowString(10, 290, 100, 16, 16, "recv dist");
+    HAL_UART_Receive(huart, &distance, 4, 0xffff); // recv distance data
+    LCD_ShowString(10, 290, 100, 16, 16, "recv ack2");
     HAL_UART_Receive(huart, &receive_buffer, 1, 0xffff); // recv ACK2
-	LCD_ShowString(10, 290, 100, 16, 16, "wait ack2");
+    LCD_ShowString(10, 290, 100, 16, 16, "wait ack2");
     handle_ack2(receive_buffer);
-	LCD_ShowString(10, 290, 100, 16, 16, "draw manual");
+    LCD_ShowString(10, 290, 100, 16, 16, "draw manual");
     draw_manual(distance);
-	LCD_ShowString(10, 290, 100, 16, 16, "done");
-    need_greeting_flag = 0;
+    LCD_ShowString(10, 290, 100, 16, 16, "done");
 }
 
 uint8_t send_manual_inst()
@@ -140,10 +139,12 @@ uint8_t send_manual_inst()
     }
     else if (button_pressed & 0b0100) // back
     {
-        opcode = 0x06;
+        opcode = 0x10;
         HAL_UART_Transmit(huart, &opcode, 1, 0xffff);
         HAL_UART_Receive(huart, &receive_buffer, 1, 0xffff);
         // LF, LB, RF, RB
+        uint8_t control[4] = {0, 0, 0, 0};
+        HAL_UART_Transmit(huart, control, 4, 0xffff);
     }
     else if (button_pressed & 0b0100) // rotR
     {
@@ -155,5 +156,5 @@ uint8_t send_manual_inst()
         HAL_UART_Transmit(huart, control, 4, 0xffff);
     }
     HAL_UART_Receive(huart, &receive_buffer, 1, 0xffff);
-	return 0x00;
+    return 0x00;
 }
--- LeiJun/Core/Src/gui.c
+++ LeiJun/Core/Src/gui.c
@@ -3,6 +3,8 @@
 #include <string.h>
 
 extern uint8_t LeiJun_mode;
+extern uint8_t waypoint_state = 0;
+uint16_t obstacles = 0x0000;
 
 void draw_button_text(uint16_t sx, uint16_t sy, uint16_t ex, uint16_t ey, uint8_t text_size, const char *text,
                       button_area *button)
@@ -18,14 +20,23 @@ void draw_button_text(uint16_t sx, uint16_t sy, uint16_t ex, uint16_t ey, uint8_
     LCD_ShowString_trans(text_sx, text_sy, text_size * strlen(text) >> 1, text_size, text_size, text);
 }
 
+void draw_button_color(uint16_t sx, uint16_t sy, uint16_t ex, uint16_t ey, const uint16_t color, button_area *button)
+{
+    button->ex = ex;
+    button->ey = ey;
+    button->sx = sx;
+    button->sy = sy;
+    LCD_Fill_Window(sx, sy, ex, ey, color);
+}
+
 void draw_manual(float distance)
 {
-    LCD_ShowString(64, 10, 176, 16, 16, "Manual Control");
+    LCD_ShowString(64, 10, 150, 16, 16, "Manual Control");
     draw_button_text(30, 60, 90, 120, 16, "TurnL", (button_manual + 0));
     draw_button_text(90, 60, 150, 120, 16, "Forward", (button_manual + 1));
     draw_button_text(150, 60, 210, 120, 16, "TurnR", (button_manual + 2));
     draw_button_text(30, 120, 90, 180, 16, "Left", (button_manual + 3));
-    draw_button_text(90, 120, 150, 180, 16, "Forward", (button_manual + 4));
+    draw_button_text(90, 120, 150, 180, 16, "Stop", (button_manual + 4));
     draw_button_text(150, 120, 210, 180, 16, "Right", (button_manual + 5));
     draw_button_text(30, 180, 90, 240, 16, "RotL", (button_manual + 6));
     draw_button_text(90, 180, 150, 240, 16, "Back", (button_manual + 7));
@@ -34,12 +45,39 @@ void draw_manual(float distance)
         char distance_buffer[50];
         sprintf(distance_buffer, "Distance: %.1f cm", distance);
         LCD_Fill_Window(10, 250, 100, 267, WHITE);
-		POINT_COLOR = BLACK;
+        POINT_COLOR = BLACK;
         LCD_ShowString(10, 250, 200, 16, 16, distance_buffer);
     }
 }
 
 void draw_waypoint()
 {
-
+    LCD_ShowString(56, 10, 150, 16, 16, "Waypoint Control");
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            char button_text[2];
+            sprintf(button_text, "%d", i * 4 + j);
+            if (obstacles & (1 << (i * 4 + j))) {
+                draw_button_color(20 + j * 50, 60 + i * 50, 70 + j * 60, 110 + i * 50, RED, (button_map + i * 4 + j));
+            }
+            else {
+                draw_button_color(20 + j * 50, 60 + i * 50, 70 + j * 60, 110 + i * 50, GREEN, (button_map + i * 4 + j));
+            }
+        }
+    }
+    char mode_text[20];
+    if (waypoint_state == 0) {
+        sprintf(mode_text, "Set Start");
+    }
+    else if (waypoint_state == 1) {
+        sprintf(mode_text, "Set End");
+    }
+    else if (waypoint_state == 2) {
+        sprintf(mode_text, "Set Obstacles");
+    }
+    else if (waypoint_state == 3) {
+        sprintf(mode_text, "Set Waypoints");
+    }
+    POINT_COLOR = BLACK;
+    LCD_ShowString(10, 270, 200, 16, 16, mode_text);
 }
